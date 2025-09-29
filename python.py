731f50d5267dd0efdf5a10010897fa44a0f35688
EdgarUrielMoralesTorres <23308060610613@cetis61.edu.mx>
2025-09-29 07:24:20 -0500

    test

--- python.py
+++ python.py
@@ -1,13 +1,17 @@
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    mensaje= "test"
-    return mensaje
+    return "test"
 
-@app.route('/factorial')
+@app.route('/factorial/<v1>')
 def factorial(v1):
   f=1
-    
+  for x in range(1,6):
+    f*=x
+  return (f("el factorial de {v1}! es {f}"))
+
 if __name__ == '__main__':
   app.run(debug=True)
